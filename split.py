3dcdd0a25a9847f2474876fe92c8012c7eddd9a6
William Fanchiang <wl.fanchiang@gmail.com>
2024-03-23 20:13:00 +0800

    讀取檔案及寫入特定檔案

--- split.py
+++ split.py
@@ -1,31 +1,30 @@
 #讀取檔案
-description = []
-description1 = []
-with open('wp1.csv', 'r', encoding = 'utf-8') as file: #read 也是需要相同的編碼方可讀取出正確的資料內容
+description0 = []
+description1 = [] 
+description2 = []
+n = 0
+with open('wp1.csv', 'r', encoding='utf-8') as file: #read 也是需要相同的編碼方可讀取出正確的資料內容
 	for line in file:
+		if '商品,價錢' in line: #Boolean->True或是Fail 
+			continue #滿足上述boolean值 Continue繼續表示直接進入'line #1'
 		a = line.split(',') # split為分割，並已',' 作為區分的指標
-		print(a)
+		print('這是a的，line中第',n + 1, a)
 		print('----------------')
-		a = line.strip().split(',') # 注意執行順序 #1將line中移除\n；#2 再以','作為區分；#3 最後再存入 a str字串中。
-		print(a)
+		b = line.strip().split(',') # 注意執行順序 #1將line中移除\n；#2 再以','作為區分；#3 最後再存入 a str字串中。
+		print('這是b的，line中第',n + 1, b)
 		print('----------------')
 		name, price = line.strip().split(',') # 得知透過strip()與split已','作為區分後，line已經被拆分為[0], [1]清單，我們透過name, price 宣稱 = [0], [1] 
-
-
-
-description.append(a)
-description1.append([name, price])
+		description2.append([name, price])
+		n += 1
+description0.append(a)
+description1.append(b)
+description2.append([name, price])
 print('final --------------')
-print(description)
+print(description0)
 print(description1)
+print(description2)
 
-
-
-
-
-
-
-
+# 寫入特定檔案
 products = []
 while True: #強制進入loop環節；(boolen-> True or fail)
 	item = input('商品名稱: ')
